8bac9a566837d9b311da39face19e1a0cd866276
jwpettit <jwpettit@me.com>
2023-04-09 16:41:33 -0700

    Added some notes for studying purposes

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // This feels to me like it is the equivalent of the Routers in React
     var body: some View {
         TabView {
             Text("Home Page")
@@ -16,7 +17,9 @@ struct ContentView: View {
                     Image(systemName: "cup.and.saucer")
                     Text("Home Page")
                 }
+            // Notice how here we just are putting in the component
             OffersPage()
+                // Seems these modifiers then create the tabs as children of TabView
                 .tabItem {
                     Image(systemName: "tag")
                     Text("Offers")
