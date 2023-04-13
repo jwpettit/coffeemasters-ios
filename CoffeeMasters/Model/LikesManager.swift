cf7da2f30b5672ca76b9a6bf97a162546a1f7810
jwpettit <jwpettit@me.com>
2023-04-12 17:42:23 -0700

    Created LikesManager

---
+++ CoffeeMasters/Model/LikesManager.swift
@@ -0,0 +1,32 @@
+//
+//  LikesManager.swift
+//  CoffeeMasters
+//
+//  Created by Jeffrey Pettit on 4/12/23.
+//
+
+import Foundation
+
+class LikesManager: ObservableObject {
+    
+    static let key = "likes"
+    
+    @Published var likes: [Int] = []
+    
+    init() {
+        likes = UserDefaults.standard.array(forKey: LikesManager.key) as? [Int] ?? []
+    }
+    
+    func isLiked(id: Int) -> Bool {
+        return likes.contains(id)
+    }
+    
+    func toggle(_ id: Int) {
+        if isLiked(id: id) {
+            likes.removeAll() { $0 == id }
+        } else {
+            likes.append(id)
+        }
+        UserDefaults.standard.set(likes, forKey: LikesManager.key)
+    }
+}
